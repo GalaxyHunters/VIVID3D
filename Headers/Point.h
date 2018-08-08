cb21704a6a35bfb52e01b4ffedfd48a658f41559
OzW1234 <32037231+OzW1234@users.noreply.github.com>
2018-08-08 15:08:16 +0300

    Update Point.h

--- Headers/Point.h
+++ Headers/Point.h
@@ -1,13 +1,18 @@
 #pragma once
+#include <cmath>
 
 class Point
 {
 public:
 	Point(float x, float y, float z);
 	~Point();
+	double distance(Point p) { return sqrt(pow(this->_x - p.getX(), 2) + pow(this->_y - p.getY(), 2) + pow(this->_z - p.getZ(), 2)); }
 	float getX() { return this->_x; }
 	float getY() { return this->_y; }
 	float getZ() { return this->_z; }
+	void setX(float x) { this->_x = x; }
+	void setY(float y) { this->_y = y; }
+	void setZ(float z) { this->_z = z; }
 	//float getDis(Point p) { return (); }
 private:
 	float _x, _y, _z;

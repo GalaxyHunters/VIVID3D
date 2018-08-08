098afd7020fae2ce2aa2280e2600aec501b45ac9
OzW1234 <32037231+OzW1234@users.noreply.github.com>
2018-08-08 14:15:41 +0300

    Delete Vivid.h

--- Headers/Vivid.h
+++
@@ -1,8 +0,0 @@
-#pragma 
-#include "SimSurf.h"
-
-static Surf importObj(string input);
-static void vivify(float** points, bool mask[], float quan[], float Vmax, float Vmin, string label, int alpha);
-
-
-

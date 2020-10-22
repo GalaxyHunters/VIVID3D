77aba57f53e868dd5c96bb2ad98b67c61867f7fa
raphae2118 <raphaelbuzaglo@gmail.com>
2020-10-22 06:11:51 +0300

    Working viewer import and tweaks to my cmake
    added set(PYTHON_EXECUTABLE "path") to my cmake for pybind to locate python.
    added a working vivid.CMake.show() that prints a stub.
    im committing this at 6am before idf ^-^

--- viewer_py/viewer.py
+++ viewer_py/viewer.py
@@ -6,45 +6,41 @@ and jupyter notebooks using model-viewer
 """
 import os
 import base64
+import io
 
-def encode_html(glb):
+def encode_html(path):
     """
     Return HTML that will render the scene using
     GLTF/GLB encoded to base64 loaded by model-viewer
-    #TODO run in python 2.7
-    #TODO run with
     Parameters
     --------------
-    :param glb: glb file binary
-
+    model: .glb file 
+    #TODO test with gltf
     Returns
     --------------
     html : str
       HTML containing embedded geometry
-
     """
     # fetch HTML template
     with open('template.html', 'r', encoding="utf8") as f:
         base = f.read()
     # get export as bytes
-    # with open(path,'rb') as model:
-    #    data = model.read()
-    data = glb
+    with open(path,'rb') as model:
+        data = model.read()
     # encode as base64 string
-    encoded = "data:image/jpeg;base64,{model}".format(model=base64.b64encode(data).decode('utf-8'))
+    encoded = "data:image/jpeg;base64,{model}".format(model=base64.b64encode(data).decode('utf-8')) 
     # replace keyword with our scene data
     result = base.replace('$B64GLTF', encoded) # $B64GLTF has no meaning just a placeholder in the template file
     return result
-
-
 def show(path, height=500, **kwargs):
     """
-    Convert a glb to HTML containing embedded geometry
-    and a module viewer instance that will display nicely in
+    Convert a scene to HTML containing embedded geometry
+    and a three.js viewer that will display nicely in
     an IPython/Jupyter notebook.
     Parameters
     -------------
-    Srt : path to the glb file
+    scene : trimesh.Scene
+      Source geometry
     Returns
     -------------
     html : IPython.display.HTML
@@ -58,7 +54,7 @@ def show(path, height=500, **kwargs):
 
     # escape the quotes in the HTML
     srcdoc = as_html.replace('"', '&quot;')
-
+    
     # embed this puppy as the srcdoc attr of an IFframe
     embedded = display.HTML(
         '<iframe srcdoc="{srcdoc}" '
@@ -67,3 +63,8 @@ def show(path, height=500, **kwargs):
             srcdoc=srcdoc,
             height=height))
     return embedded
+
+
+
+if __name__ == "__main__":
+    show('model.glb')
--- viewer_py/vivid.py
+++ viewer_py/vivid.py
@@ -1,25 +1,13 @@
 from vivid_py import *
 import viewer
 
-from functools import wraps  # This convenience func preserves name and docstring
 
-
-def add_method(cls):
-    def decorator(func):
-        @wraps(func)
-        def wrapper(self, *args, **kwargs):
-            return func(*args, **kwargs)
-
-        setattr(cls, func.__name__, wrapper)
-        # Note we are not binding func, but wrapper which accepts self but does exactly the same as func
-        return func  # returning func means func can still be used normally
-
-    return decorator
-
-
-@add_method(CModel)
-def show(self):
+def show(a):
     # print(self.ExportToGlb())
+    #if a.ExportToGlb():
+    embedded = viewer.show('model.glb')
     # viewer.show(self.ExportToGlb())
-    a = self.ExportToGlb()
-    return a
+    #stub = a.ExportToGlb()
+    #return stub
+    return embedded
+setattr(CModel,'show',show)

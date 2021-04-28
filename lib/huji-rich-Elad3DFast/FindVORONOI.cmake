9f7a48425b908336c16c08fe9551e3ea6ef627c1
ghil <tussbaum@gmail.com>
2021-04-28 14:11:29 +0300

    merged Zohar and Tomer

--- lib/huji-rich-Elad3DFast/FindVORONOI.cmake
+++ lib/huji-rich-Elad3DFast/FindVORONOI.cmake
@@ -12,42 +12,42 @@ IF(NOT TARGET VoronoiLib)
     add_library(VoronoiLib SHARED
                 #elad3d
                 #source/3D/GeometryCommon
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/Voronoi3D.cpp
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/Delaunay3D.cpp
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/Face.cpp
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/HilbertOrder3D.cpp
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/HilbertOrder3D_Utils.cpp
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/Intersections.cpp
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/predicates.cxx
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/Predicates3D.cpp
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/RoundGrid3D.cpp
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/Tessellation3D.cpp
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/tetgen.cxx
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/Tetrahedron.cpp
-                Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon/Vector3D.cpp
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/Voronoi3D.cpp
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/Delaunay3D.cpp
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/Face.cpp
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/HilbertOrder3D.cpp
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/HilbertOrder3D_Utils.cpp
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/Intersections.cpp
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/predicates.cxx
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/Predicates3D.cpp
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/RoundGrid3D.cpp
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/Tessellation3D.cpp
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/tetgen.cxx
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/Tetrahedron.cpp
+                lib/huji-rich-Elad3DFast/source/3D/GeometryCommon/Vector3D.cpp
                 #source/3D/r3d
-                Libs/huji-rich-Elad3DFast/source/3D/r3d/Intersection3D.cpp
-                Libs/huji-rich-Elad3DFast/source/3D/r3d/r3d.c
+                lib/huji-rich-Elad3DFast/source/3D/r3d/Intersection3D.cpp
+                lib/huji-rich-Elad3DFast/source/3D/r3d/r3d.c
                 #source/misc
-                Libs/huji-rich-Elad3DFast/source/misc/bisection.cpp
-                Libs/huji-rich-Elad3DFast/source/misc/func_1_var.cpp
-                Libs/huji-rich-Elad3DFast/source/misc/horner.cpp
-                Libs/huji-rich-Elad3DFast/source/misc/int2str.cpp
-                Libs/huji-rich-Elad3DFast/source/misc/io3D.cpp
-                Libs/huji-rich-Elad3DFast/source/misc/mesh_generator.cpp
-                Libs/huji-rich-Elad3DFast/source/misc/mesh_generator3D.cpp
-                Libs/huji-rich-Elad3DFast/source/misc/serializable.cpp
-                Libs/huji-rich-Elad3DFast/source/misc/simple_io.cpp
-                Libs/huji-rich-Elad3DFast/source/misc/universal_error.cpp
-                Libs/huji-rich-Elad3DFast/source/misc/utils.cpp
+                lib/huji-rich-Elad3DFast/source/misc/bisection.cpp
+                lib/huji-rich-Elad3DFast/source/misc/func_1_var.cpp
+                lib/huji-rich-Elad3DFast/source/misc/horner.cpp
+                lib/huji-rich-Elad3DFast/source/misc/int2str.cpp
+                lib/huji-rich-Elad3DFast/source/misc/io3D.cpp
+                lib/huji-rich-Elad3DFast/source/misc/mesh_generator.cpp
+                lib/huji-rich-Elad3DFast/source/misc/mesh_generator3D.cpp
+                lib/huji-rich-Elad3DFast/source/misc/serializable.cpp
+                lib/huji-rich-Elad3DFast/source/misc/simple_io.cpp
+                lib/huji-rich-Elad3DFast/source/misc/universal_error.cpp
+                lib/huji-rich-Elad3DFast/source/misc/utils.cpp
                 #extra
-                Libs/huji-rich-Elad3DFast/source/tessellation/geometry.cpp
+                lib/huji-rich-Elad3DFast/source/tessellation/geometry.cpp
                 )
 
 
     # Telling cmake where to find headers for the code.
-    target_include_directories(VoronoiLib PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/Libs/huji-rich-Elad3DFast/source/3D/GeometryCommon)
-    target_include_directories(VoronoiLib PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/Libs/huji-rich-Elad3DFast/source/misc)
+    target_include_directories(VoronoiLib PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/lib/huji-rich-Elad3DFast/source/3D/GeometryCommon)
+    target_include_directories(VoronoiLib PUBLIC ${CMAKE_CURRENT_SOURCE_DIR}/lib/huji-rich-Elad3DFast/source/misc)
 
     MESSAGE(CREATED_VORONOI)
 

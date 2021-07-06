869f0c5cd0dc799071515ea308ea814e7b003f2e
Beilinson <adam.v.beili@gmail.com>
2021-07-06 22:20:09 +0300

    Refactor of Shapes Added CreateCubeMesh() Added alt CreateEllipsoidByTransformMesh (needs to be tested)

--- src/ModelBuilder/Shapes.h
+++ src/ModelBuilder/Shapes.h
@@ -12,24 +12,30 @@ namespace vivid
 
 // TODO: get ride of size_t
 
-// TODO add cubeMesh (use boxMesh with sizeX... are all one param
 // TODO These functions should be static and go by the naming conventions (and better naming, consistant naming...)
 // TODO also, use Doxygen commenting standards
 // TODO make sure all the code parts here is consistent, names of param are sometime capitalized, some not... the order of actions in the algorithm, the inner naming etc...
 // all 3 vec should be passed with CPoint const&
 
 
-CMesh CreateBoxMesh(double sizeX, double sizeY, double sizeZ, coord_t color, coord_t alpha, std::vector<double> position);
-/*Creates a cube*/
+CMesh CreateCubeMesh(double aSize, coord_t aColor, coord_t aAlpha, const CPoint &arCenter);
+/*Creates a Cube*/
 
-CMesh CreateSphereMesh(std::size_t num_of_meridians, std::size_t num_of_parallels, double radius, const CPoint &arCenter, coord_t Color, coord_t Alpha, const std::string Label);
+CMesh CreateBoxMesh(double sizeX, double aSizeY, double aSizeZ, coord_t aColor, coord_t aAlpha, const CPoint &arCenter);
+/*Creates a Box*/
+
+CMesh CreateSphereMesh(std::size_t aNumOfMeridians, std::size_t aNumOfParallels, double aRadius, const CPoint &arCenter, coord_t aColor, coord_t aAlpha, const std::string aLabel);
 /*Creates a sphere*/
 
-CMesh CreateEllipsoidMesh(std::size_t NumOfMeridians, std::size_t NumOfParallels, std::vector<double> aRadiusVec,
-                                 std::vector<double> CenterPoint, std::vector<double> MajorAxis, std::vector<double> MiddleAxis, std::vector<double> MinorAxis,
-                                 coord_t Color, coord_t Alpha, std::string Label);
+CMesh CreateEllipsoidMesh(std::size_t aNumOfMeridians, std::size_t aNumOfParallels, std::vector<double> aRadiusVec,
+                          const CPoint &arCenter, std::vector<double> aMajorAxis, std::vector<double> aMiddleAxis, std::vector<double> aMinorAxis,
+                          coord_t aColor, coord_t aAlpha, std::string aLabel);
 
-CMesh CreateArrowMesh(double Width, double PCRatio, std::vector<double> aPos, std::vector<double> DirVec, double Color, double Alpha, std::string Label);
+CMesh CreateEllipsoidByTransformMesh(size_t aNumOfMeridians, size_t aNumOfParallels, vector<double> aRadiusVec,
+                                     const CPoint &arCenter, vector<double> aMajorAxis, vector<double> aMiddleAxis,
+                                     vector<double> aMinorAxis, coord_t aColor, coord_t aAlpha, string aLabel);
+
+CMesh CreateArrowMesh(double aWidth, double aPCRatio, const CPoint &arCenter, std::vector<double> aDirVec, double aColor, double aAlpha, std::string aLabel);
 /*Creates a arrow*/
 
 } // namespace vivid

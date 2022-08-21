9efc5da91e595b9423a3a2f2058a0f79fd44860e
GalaxyHuntersIL <naftalide@gmail.com>
2022-08-21 03:50:50 -0700

    Deleted useless files

--- build/temp.linux-x86_64-3.9/CMakeFiles/vivid.dir/src/ModelBuilder/VoronoiSurface.cpp.o.d
+++
@@ -1,4 +0,0 @@
-CMakeFiles/vivid.dir/src/ModelBuilder/VoronoiSurface.cpp.o: \
- /home/naf/projects/Vivid/Vivid/src/ModelBuilder/VoronoiSurface.cpp \
- /usr/include/stdc-predef.h \
- /home/naf/projects/Vivid/Vivid/src/ModelBuilder/VoronoiSurface.h

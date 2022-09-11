703447ed8ae7f0481cae64fb4c5d943868bfe5fc
GalaxyHuntersIL <naftalide@gmail.com>
2022-09-11 06:27:08 -0700

    assimp source added for better build on multiple machines

---
+++ lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/DependInfo.cmake
@@ -0,0 +1,33 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/adler32.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/adler32.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/adler32.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/compress.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/compress.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/compress.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/crc32.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/crc32.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/crc32.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/deflate.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/deflate.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/deflate.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/gzclose.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/gzclose.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/gzclose.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/gzlib.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/gzlib.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/gzlib.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/gzread.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/gzread.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/gzread.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/gzwrite.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/gzwrite.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/gzwrite.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/infback.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/infback.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/infback.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/inffast.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/inffast.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/inffast.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/inflate.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/inflate.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/inflate.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/inftrees.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/inftrees.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/inftrees.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/trees.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/trees.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/trees.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/uncompr.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/uncompr.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/uncompr.c.o.d"
+  "/home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib/zutil.c" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/zutil.c.o" "gcc" "lib/assimp/contrib/zlib/CMakeFiles/zlibstatic.dir/zutil.c.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ lib/assimp/contrib/zlib/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib
+# Build directory: /home/naf/projects/Vivid/Vivid/lib/assimp/contrib/zlib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.

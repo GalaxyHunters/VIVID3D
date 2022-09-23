a95119faf676de56002c857935e9a8da7ec82cc7
Beilinson <adam.v.beili@gmail.com>
2022-09-23 16:55:11 -0700

    added to wrapper, updated version

--- src/Utils/LogFile.h
+++ src/Utils/LogFile.h
@@ -13,9 +13,26 @@
 using namespace std;
 
 namespace vivid {
+    /**
+     * Smooth faces on CMesh by Laplacian Smooth
+     * @param[in] aCode LOG_ERROR | LOG_WARNING | LOG_VIVID | LOG_INFO | LOG_DEBUG
+     * @param[in] aMsg predefined message code
+     */
     void Log(const ELogCode aCode, const ELogMessage aMsg);
+    /**
+     * Log a custom message to the VIVID Logger
+     * @param[in] aCode LOG_ERROR | LOG_WARNING | LOG_VIVID | LOG_INFO | LOG_DEBUG
+     * @param[in] arMsg custom message string
+     */
     void Log(const ELogCode aCode, const string& arMsg);
 
+    /**
+     * Configure VIVID in-built Logging Process
+     * @param[in] aWriteToConsole select whether to print to console
+     * @param[in] aLogLevel weighted value for smoothing, recommended range 0.2<a<0.8
+     * @param[in] aWriteToFile select whether to write to file
+     * @param[in] arLogFileName custom file to write to. Default file is "./VIVID_LOG.txt"
+     */
     void ConfigLogging(bool aWriteToConsole, const ELogCode aLogLevel, bool aWriteToFile, const string& arLogFileName);
 }
 #endif //VIVID_LOGFILE_H

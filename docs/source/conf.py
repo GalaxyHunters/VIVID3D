cecd74ffb9cac68d2dbb9c63d7a1df0058c195fe
GalaxyHuntersIL <naftalide@gmail.com>
2022-10-22 12:40:42 -0700

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -22,7 +22,7 @@ copyright = '2022, GalaxyHuntersIL'
 author = 'GalaxyHuntersIL'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.5'
+release = '0.2.7'
 
 
 # -- General configuration ---------------------------------------------------

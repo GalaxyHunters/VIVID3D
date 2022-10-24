bdf1ebc32023cf9973c80848fc06f01c9fa05e71
Adam <adam.v.beili@gmail.com>
2022-10-24 21:20:41 +0300

    Fixed Viewer data encoding

---
+++ src/Python/viewer/viewer.py
@@ -0,0 +1,46 @@
+"""
+html model generator
+-------------
+Render gltf objects in HTML
+and jupyter notebooks using model-viewer
+"""
+import base64
+
+def show(glb):
+    """
+    Convert a scene to HTML containing embedded geometry
+    and a three.js viewer that will display nicely in
+    an IPython/Jupyter notebook.
+    Parameters
+    -------------
+    glb : binary
+      .glb blob file
+    Returns
+    -------------
+    html : IPython.display.HTML
+      Object containing rendered scene
+    """
+    # keep as soft dependency
+    from IPython import display
+
+    # convert scene to a full HTML page
+    # fetch HTML template
+    with open('template.html', 'r', encoding="utf8") as f:
+        template = f.read()
+    # get export as bytes
+    # with open(path,'rb') as model:
+    #     data = model.read()
+    # encode as base64 string
+    encoded = base64.b64encode(glb).decode('utf-8')
+    # replace keyword with our scene data
+    as_html = template.replace('$B64GLTF', encoded) # $B64GLTF has no meaning just a placeholder in the template file
+
+    # escape the quotes in the HTML
+    srcdoc = as_html.replace('"', '&quot;')
+    
+    # embed this puppy as the srcdoc attr of an IFframe
+    embedded = display.HTML(
+        '<iframe srcdoc="{srcdoc}" '
+        'width="100%" height="500px" '
+        'style="border:none;"></iframe> '.format(srcdoc=srcdoc))
+    return embedded

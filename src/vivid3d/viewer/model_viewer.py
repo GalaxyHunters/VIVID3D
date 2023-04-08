f828e9414a065dd52d73eea300141057acdf84b6
Adam Beili <adam.v.beili@gmail.com>
2023-04-08 20:33:34 +0300

    Materials

--- src/vivid3d/viewer/model_viewer.py
+++ src/vivid3d/viewer/model_viewer.py
@@ -37,6 +37,7 @@ def view_glb(glb, height=600):
     template_path = os.path.join(os.path.dirname(__file__), 'template.html')
     with open(template_path, 'r', encoding="utf-8") as f:
         template = f.read()
+    # This increases file size by ~30%.
     encoded = base64.b64encode(glb).decode('utf-8')
     # replace keyword with our scene data
     srcdoc = template.replace('$B64GLTF', encoded)
@@ -65,8 +66,8 @@ def show(model, height=600):
 
     Parameters
     ----------
-    model : vivid3d.Model or vivid3d.BaseMesh
-        .glb encoded blob file
+    model : 
+        vivid3d.Model or vivid3d.BaseMesh
     height : int, default: 600
         height in pixels to open the viewer
 
@@ -75,6 +76,11 @@ def show(model, height=600):
     html : string
       The HTML page
 
+    Notes
+    -----
+    The viewer works best in Chromium browsers. Firefox is limited to small models (<32Mb).
+    Models larger than >100Mb may not load. If your model is too large, you can compress individual meshes using the .decimate() method.
+    If you are experiencing further issues, we recommend exporting the model using .export() and dragging the file into the viewer window.
     """
     try:
         glb = model.export_to_blob(file_type="glb")
